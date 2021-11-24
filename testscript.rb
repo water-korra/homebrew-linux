6ab1ddd92f70f6b36f9b85381d823ef29e77e29f
water-korra <uberigloo@gmail.com>
2021-11-24 21:20:13 +0200

    third commit

--- testscript.rb
+++ testscript.rb
@@ -5,9 +5,9 @@ class Testscript < Formula
   sha256 "ff5b389080b2825ed64e2a5eae87ef90515ba5c47f5c824a390be6c3198d1a6d"
 
 
-      def install
-	bin.install "testscript'
-      end
+    def install
+	bin.install "testscript"
+     end
 
 
 end
