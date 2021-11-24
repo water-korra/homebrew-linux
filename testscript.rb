73d809ef304c795f03815a439172073bc9c4caf1
water-korra <uberigloo@gmail.com>
2021-11-24 21:12:33 +0200

    first commit

---
+++ testscript.rb
@@ -0,0 +1,14 @@
+class Testscript < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/water-korra/testscript/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "ff5b389080b2825ed64e2a5eae87ef90515ba5c47f5c824a390be6c3198d1a6d"
+
+
+  def install
+	bin.install "testscript'
+  end
+
+
+  end
+
